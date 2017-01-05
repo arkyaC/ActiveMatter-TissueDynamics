7399783ca377696d4fe9d746526b75fa3f4ac9c4
arkyaC <arkya.chat@gmail.com>
2017-01-05 10:07:33 +0530

    error fixed

--- vicsek.m
+++ vicsek.m
@@ -85,8 +85,9 @@ end
 % axis([-pi,pi,0,100]);
 
 % write data to dump
+timeSteps = 1:Nsteps;
 fileID = fopen('dump.txt','w');
-fprintf(fileID,'%d \t %6.5f \n',(1:Nsteps),orderN);
+fprintf(fileID,'%d \t %6.5f \n',[timeSteps;orderN]);
 fclose(fileID);
 
 %plot order parameter against time
