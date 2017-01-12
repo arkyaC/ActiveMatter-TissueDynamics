86f3a16bceda560e040882e69080e9c856498155
arkyaC <arkya.chat@gmail.com>
2017-01-12 12:26:10 +0530

    removed square root

--- vicsek.m
+++ vicsek.m
@@ -1,5 +1,5 @@
 function orderN = vicsek(rho,noise)
-numberOfPoints = 40;
+numberOfPoints = 4000;
 %rho = 10.4;
 L = sqrt(numberOfPoints/rho);
 %noise = .4;
@@ -8,20 +8,19 @@ r = 1; %definition of neighbourhood for averaging
 
 keeperX =L* rand(1, numberOfPoints);
 keeperY =L* rand(1, numberOfPoints);
-Nsteps=10000;cutOffIter=Nsteps-200;
+Nsteps=15000;
 theta = 2*pi*rand(1,numberOfPoints) - pi; %-pi to pi
 %theta = pi/2 * ones(1,numberOfPoints);
 timedelta = 1; %as mentioned in paper
 y = zeros(Nsteps+1,3*numberOfPoints);
 y(1,:) = [keeperX,keeperY,theta];
-correl = 0; %to be evaluated later
-counter = 0;
 orderN = zeros(1,Nsteps);
 for k=1:Nsteps
 	posX=y(k,1:numberOfPoints); % x position matrix
 	posY=y(k,numberOfPoints+1:2*numberOfPoints); % y position matrix
-	theta=y(k,2*numberOfPoints+1:end);
-    newTheta=theta; %just arbit initialisation
+	theta=y(k,2*numberOfPoints+1:end); % theta matrix
+    
+    newTheta=theta; %just arbit initialisation, supposed to be faster this way
 	vel=[v*cos(theta) v*sin(theta)];
     s = [0,0]; %avg vel init
     for i=1:numberOfPoints %evaluating updated angle parameter
@@ -30,8 +29,8 @@ for k=1:Nsteps
         ni = [vel(i) vel(numberOfPoints+i)]/v;
         s = s + ni;
         for j=1:numberOfPoints
-            distIJ = sqrt((posX(j)-posX(i))^2 + (posY(j) - posY(i))^2);
-            if distIJ<r
+            distIJsq = (posX(j)-posX(i))^2 + (posY(j) - posY(i))^2;
+            if distIJsq<=(r^2)
                 avg = avg + theta(j);
                 ctr = ctr + 1;
             end
@@ -39,26 +38,22 @@ for k=1:Nsteps
         avg = avg/ctr;
         temp = avg + (noise*rand - noise/2);
         %keep angle between -pi and pi
-        newTheta(i) = temp - 2*pi*floor( (temp+pi)/(2*pi) );
-        %newTheta(i) = wrapToPi(temp);
-        %newTheta(i) = 2*pi * (temp/(2*pi) - floor(temp/(2*pi)));
+        newTheta(i) = temp-2*pi*floor((temp+pi)/(2*pi));
+%         newTheta(i) = wrapToPi(temp);
+%         newTheta(i) = 2*pi * (temp/(2*pi) - floor(temp/(2*pi)));
     end
-    
     orderN(k)=sqrt(s(1)^2+s(2)^2)/numberOfPoints;
-    if k>cutOffIter
-        correl = correl + orderN(k);
-        counter = counter + 1;
-    end
+    
+% visualising distribution of directions
 %     histogram(theta,100);
 %     axis([-pi,pi,0,100]);
 %     pause(0.05);
-    theta = newTheta;
-    %vel = [v*cos(theta) v*sin(theta)]; %updated vel
+    theta = newTheta; % update angle values
     posX = posX + vel(1:numberOfPoints)*timedelta;
     posY = posY + vel(numberOfPoints+1:end)*timedelta;
-    posX = L*(posX/L-floor(posX/L));
-	posY = L*(posY/L-floor(posY/L));
-    y(k+1,:)=[posX,posY,theta];%time stepping
+    posX = posX-L*floor(posX/L); % periodic BC
+	posY = posY-L*floor(posY/L);
+    y(k+1,:)=[posX,posY,theta];
 end
 % histogram(theta,100);
 % axis([-pi,pi,0,100]);
@@ -70,9 +65,9 @@ end
 % fclose(fileID);
 
 %plot order parameter against time
-% plot(linspace(0,Nsteps,Nsteps),orderN);
-% axis([0,Nsteps,0,1]);
-% xlabel('Time step');ylabel('Order Parameter');
+plot(linspace(0,Nsteps,Nsteps),orderN);
+axis([0,Nsteps,0,1]);
+xlabel('Time step');ylabel('Order Parameter');
 
 % pause(10);
 
