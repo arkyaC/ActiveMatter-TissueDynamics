50499a12ecf32de9b37927f3e0f2ad77c4489368
arkyaC <arkya.chat@gmail.com>
2017-02-28 19:54:05 +0530

    updated periodic BC

--- noisyNucl.m
+++ noisyNucl.m
@@ -1,18 +1,19 @@
-function correl=noisyNucl(rhoNorm)
+function orderN=noisyNucl(rhoNorm,noise)
 %rhoNorm = 0.1;
 %L=14;
 driftspeed = 1;
 mu = 1;
 tau = 1;
 Req = 5/6;R0 = 1;
-Fadh = 0.75/6;Frep = 5;Fwall = 50/6;
-minAllowableDistance = R0;
+Fadh = 0.75/6;Frep = 5;%Fwall = 50/6;
+% Fadh = 0.75;Frep = 30;%Fwall = 50;
+minAllowableDistance = 0.01; %arbitrary
 %numberOfPoints = floor(2*rhoNorm*L^2/3.14);
 numberOfPoints = 1000;
-L = sqrt(numberOfPoints*pi/(2*rhoNorm));
+L = R0 * sqrt(numberOfPoints/(2*rhoNorm));
 x = L* rand(1, 10000);
 y = L* rand(1, 10000);
-noise = 0.6; %stochastic noise parameter
+%noise = 0.6; %stochastic noise amplitude
 
 % Initialize first point.
 keeperX = x(1);
@@ -35,84 +36,101 @@ while counter<numberOfPoints
     k=k+1;
 end
 
-numberOfPoints = length(keeperX);
-Nsteps=6000;cutoffIter=Nsteps-100;
+% numberOfPoints = length(keeperX); % redundant
+Nsteps=3000;
+% cutoffIter=Nsteps-100;
 theta = 2*pi*rand(1,numberOfPoints) - pi; %need it in the range of -pi to pi
 timedelta=0.05*R0/driftspeed;
 
 y=zeros(Nsteps+1,3*numberOfPoints);
 y(1,:) = [keeperX,keeperY,theta];
 %vel=zeros(1,2*numberOfPoints);
-correl=0;
-orderN = zeros(Nsteps);
+orderN = zeros(1,Nsteps);
 for k=1:Nsteps
 	posX=y(k,1:numberOfPoints); % x position matrix
 	posY=y(k,numberOfPoints+1:2*numberOfPoints); % y position matrix
 	theta=y(k,2*numberOfPoints+1:end);
+    
 	drift=[driftspeed*cos(theta) driftspeed*sin(theta)];
     rhs = zeros(1,2*numberOfPoints);
-	for i=1:length(posX)
-		interaxn=[0,0];
-		for j=1:length(posX)
-			if j==i
-				continue;
-			end
-			dij=sqrt((posX(i)-posX(j)).^2 + (posY(i) - posY(j)).^2);
-			eij=[posX(i)-posX(j),posY(i)-posY(j)]/dij;
-			if dij<Req
-			    interaxn=interaxn-Frep*(dij-Req)/Req * eij;
-			elseif dij>=Req && dij<=R0
-			    interaxn=interaxn-Fadh*(dij-Req)/(R0-Req) * eij;
-			end
+    for i=1:numberOfPoints
+        interaxn=[0,0];
+        for j=1:numberOfPoints
+            if j==i
+                continue;
+            end
+            dx = posX(i) - posX(j);
+			dy = posY(i) - posY(j);
+            if abs(dx) > 0.5*L
+				dx = dx - L*sign(dx);
+            end
+            if abs(dy) > 0.5*L
+				dy = dy - L*sign(dy);
+            end
+            dijSq = dx^2 + dy^2;
+            dij = sqrt(dijSq);
+            eij = [dx,dy]/dij;
+%             dij=sqrt((posX(i)-posX(j)).^2 + (posY(i) - posY(j)).^2);
+%             eij=[posX(i)-posX(j),posY(i)-posY(j)]/dij;
+            if dijSq<Req^2
+                interaxn = interaxn - Frep*(dij-Req)/Req * eij;
+            elseif dijSq>=Req^2 && dijSq<=R0^2
+                interaxn = interaxn - Fadh*(dij-Req)/(R0-Req) * eij;
+            end
         end
         %for wall force
-%         Fx = 0;Fy = 0;
-%         if posX(i)<R0
-%             Fx = Fx + Fwall*exp(-2*posX(i)/R0);
-%         end
-%         if L-posX(i)<R0
-%             Fx = Fx - Fwall*exp(-2*(L-posX(i))/R0);
-%         end
-%         if posY(i)<R0
-%             Fy = Fy + Fwall*exp(-2*posY(i)/R0);
-%         end
-%         if L-posY(i)<R0
-%             Fy = Fy - Fwall*exp(-2*(L-posY(i))/R0);
-%         end
-%         interaxn = interaxn + [Fx Fy];
-		interaxn=mu*interaxn;
-		rhs(i)=drift(i)+interaxn(1);
-		rhs(numberOfPoints+i)=drift(numberOfPoints+i)+interaxn(2);
+    %         Fx = 0;Fy = 0;
+    %         if posX(i)<R0
+    %             Fx = Fx + Fwall*exp(-2*posX(i)/R0);
+    %         end
+    %         if L-posX(i)<R0
+    %             Fx = Fx - Fwall*exp(-2*(L-posX(i))/R0);
+    %         end
+    %         if posY(i)<R0
+    %             Fy = Fy + Fwall*exp(-2*posY(i)/R0);
+    %         end
+    %         if L-posY(i)<R0
+    %             Fy = Fy - Fwall*exp(-2*(L-posY(i))/R0);
+    %         end
+    %         interaxn = interaxn + [Fx Fy];
+        interaxn=mu*interaxn;
+        rhs(i)=drift(i)+interaxn(1);
+        rhs(numberOfPoints+i)=drift(numberOfPoints+i)+interaxn(2);
     end
     s=[0,0];
-    res = zeros(1,length(theta));
-	for i=1:length(theta)
-		ni=[drift(i) drift(numberOfPoints+i)]/driftspeed;
-		vi=[rhs(i) rhs(numberOfPoints+i)];
-		normvi=sqrt(vi(1)^2+vi(2)^2);
-		vi=vi/normvi;
+    res = zeros(1,numberOfPoints);
+    for i=1:numberOfPoints
+        ni=[drift(i) drift(numberOfPoints+i)]/driftspeed;
+        vi=[rhs(i) rhs(numberOfPoints+i)];
+        normvi=sqrt(vi(1)^2+vi(2)^2);
+        vi=vi/normvi;
         s=s+[vi(1) vi(2)];
         res(i)=asin(ni(1)*vi(2)-ni(2)*vi(1));
-		%res(i)=asin(dot(ez,cross(ni,vi)));
-    end
-    if k>cutoffIter
-        correl=correl+sqrt(s(1)^2+s(2)^2)/numberOfPoints; %correlation factor
     end
     orderN(k)=sqrt(s(1)^2+s(2)^2)/numberOfPoints;
-	res=(1/tau)*res;
-	posX=posX+timedelta*rhs(1:numberOfPoints);
-    posY=posY+timedelta*rhs(numberOfPoints+1:end);
-	posX=L*(posX/L-floor(posX/L));
-	posY=L*(posY/L-floor(posY/L));
-	theta=theta+timedelta*res+sqrt(timedelta*(noise^2)/12)*normrnd(0,1,[1 length(theta)]);
-	y(k+1,:)=[posX,posY,theta];%Euler stepping
+	res = (1/tau)*res;
+	posX = posX + timedelta*rhs(1:numberOfPoints);
+    posY = posY + timedelta*rhs(numberOfPoints+1:end);
+	posX = posX - L * floor(posX/L);
+	posY = posY - L * floor(posY/L);
+% 	theta = theta + timedelta*res + sqrt(timedelta*(noise^2)/12)*normrnd(0,1,[1 length(theta)]);
+    noiseAmp = noise/sqrt(timedelta);
+    theta = theta + timedelta * (res + noiseAmp * (rand(1,numberOfPoints) - 0.5));
+	y(k+1,:) = [posX,posY,theta];%Euler stepping
 end
 %plotting the order parameter value against step number
 plot(linspace(0,Nsteps,Nsteps),orderN);
 axis([0,Nsteps,0,1]);
 xlabel('Time step');ylabel('Order Parameter');
 drawnow
-%plotting the data
+
+% write data to dump
+timeSteps = 1:Nsteps;
+fileID = fopen('data/dump.txt','w');
+fprintf(fileID,'%d \t %6.5f \n',[timeSteps;orderN]);
+fclose(fileID);
+
+%movie
 % for k=1:size(y)
 % 	posX=y(k,1:numberOfPoints); % x position matrix
 % 	posY=y(k,numberOfPoints+1:2*numberOfPoints); % y position matrix
@@ -121,7 +139,7 @@ drawnow
 %     axis square;
 %     grid on;
 %     %frames(k) = getframe(gcf);
-%  	pause(.001);
+%  	pause(.05);
 % end
 
 %writing to a file
@@ -130,4 +148,3 @@ drawnow
 % fprintf(fileID,'%10s %8s %8s\n','rhoNorm','order','error');
 % fprintf(fileID,'%6.5f %5.4f %6.5f\n',A);
 % fclose(fileID);
-correl=correl/(Nsteps-cutoffIter);
--- plotRhoCorrelation.m
+++ plotRhoCorrelation.m
@@ -1,22 +1,26 @@
-rhoNorm=linspace(0,.6,11);
-rhoNorm=rhoNorm(2:end);
+% rhoNorm=linspace(0,.6,11);
+% rhoNorm=rhoNorm(2:end);
+rhoNorm = 0.6;
+noise = linspace(0,1.4,20);
+noise = noise(2:end);
 N=10;
-correl=zeros(1,length(rhoNorm));
-err=zeros(1,length(rhoNorm));
-for k=1:length(rhoNorm)
+correl=zeros(1,length(noise));
+err=zeros(1,length(noise));
+for k=1:length(noise)
     trials=zeros(1,N);
     for i=1:N
-        trials(i)=noisyNucl(rhoNorm(k));
+        orderN=noisyNucl(rhoNorm,noise(k));
+        trials(i) = mean(orderN(end-100:end));
     end
-    correl(k)=sum(trials)/N;
-    err(k)=sqrt(sum((trials-correl(k)).^2)/N);
+    correl(k)=mean(trials);
+    err(k)=sqrt(mean( (trials-correl(k)).^2 ));
 end
 %writing data to file
-A=[rhoNorm;correl;err];
-fileID = fopen('run 1.txt','w');
-fprintf(fileID,'%10s %8s %8s\n','rhoNorm','order','error');
+A=[noise;correl;err];
+fileID = fopen('data/dump.txt','w');
+fprintf(fileID,'%10s %8s %8s\n','noise','order','error');
 fprintf(fileID,'%6.5f %5.4f %6.5f\n',A);
 fclose(fileID);
 %plotting data
-% errorbar(rhoNorm,correl,err,'b.');
-% axis([0,1,0,1]);
+errorbar(rhoNorm,correl,err,'b.');
+axis([0,1,0,1]);
